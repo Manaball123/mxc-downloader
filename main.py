84b4df6189831794b96afdb2ba3a4d747a3ef51b
Mana <57663038+Manaball123@users.noreply.github.com>
2024-07-02 21:24:18 +1000

    cli

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import mxc_utils
 import json
 import requests
+import sys
 
 
 
 def main():
-    with open("sample.json", "r") as f:
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <event json filename>")
+        return
+
+    events_json_name = sys.argv[1]
+    with open(events_json_name, "r") as f:
         event = json.loads(f.read())
     url = mxc_utils.http_url_from_event(event)
     resp = requests.get(url)
@@ -13,11 +19,12 @@ def main():
     if not mxc_utils.is_encrypted_file(event):
         with open(filename, "wb") as f:
             f.write(resp.content)
+        print(f"File was unencrypted, saved as {filename}")
         return
     data = mxc_utils.decrypt_buffer(event, resp.content)
     with open(filename, "wb") as f:
         f.write(data)
-
+    print(f"File was encrypted, saved as {filename}")
 
 
 
